7a8491bd19dda9c446ace71fc684b7deeb7da708
ptotem <totem2@totem2-ThinkCentre-XXXX.(none)>
2013-04-01 15:02:53 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileDex::Application.config.secret_token = '32dd948f7e1c94a156f7c8921e081647902416a0a4069ffc37a2580c1b345849f3e5232eea6f12b18972a6b38d7de4cb9a7eb95d1ec2865f6eb0d976b1fe57fe'
