317cf3d8446fd600f14c525922ca6a6d16ae1e50
Nilesh <nilesh@ptotem.com>
2013-04-01 16:19:59 +0530

    Integrated Facebook and Twitter Omniauth. Twitter not tested yet...

---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,32 @@
+class AuthenticationsController < ApplicationController
+
+  # Create an Authentications model which belongs to User
+  # Add provider:string and uid:string to both User and Authentications.
+  # Ensure that associations and attr_accessible are correctly mapped in the Models
+  # Add the Omniauth.rb initializer
+  # Add /user/auth/:provider/callback to the routes
+  # /user/auth/:provider is handled by the Oauth gem
+
+  # Callback function of omniauth redirects back to this function.
+  # This creates a user if none exists or signs in a user if he exists
+
+  def create
+    auth=request.env["omniauth.auth"]
+    if auth.provider=='facebook' # Checking if request comes from facebook or twitter
+      if User.find_by_email(auth.info.email).nil?
+        @user = User.create!(:provider => auth.provider, :email => auth.info.email, :uid => auth.uid, :password => Devise.friendly_token[0, 20], :name => "#{auth.info.first_name} #{auth.info.last_name}")
+      else
+        @user=User.find_by_email(auth.info.email)
+      end
+    else
+      if User.find_by_uid(auth['uid']).nil?
+        @user=User.create!(:provider => auth['provider'], :uid => auth['uid'], :name => auth['info']['name'])
+      else
+        @user=User.find_by_uid(auth['uid'])
+      end
+    end
+    sign_in(:user, @user)
+    redirect_to console_path
+  end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,8 @@ module AgileDex
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.paths << "#{Rails.root}/app/assets/backgrounds"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
