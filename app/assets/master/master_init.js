1ab7ada2e977a52ecd2ffb79ecb794b9764ca5aa
Arijit Lahiri <arijit@ptotem.com>
2013-04-15 01:19:33 +0530

    Some quick refactoring

--- app/assets/master/master_init.js
+++ app/assets/master/master_init.js
@@ -4,41 +4,41 @@ var images = gon.image_list;
 
 $(function () {
     $('.ad-menu-item').slideToggle();
-    $('#title_wrap').html('<h1>' + gon.title + '</h1>');
+    var $title = $('#title_wrap');
+    $title.html('<span>' + gon.title + '</span>');
+    $title.textfill();
+    $title.fadeIn();
     if (!gon.no_subtitle) {
-        $('#subtitle').html(gon.subtitle);
-        $('#subtitle').show();
-    } else{
-        $('#subtitle_back').html('<img src='+gon.titlepic+'>');
-        $('#subtitle_back').show();
+        $('#subtitle').html(gon.subtitle).show();
+    } else {
+        $('#subtitle_back').html('<img src=' + gon.titlepic + '>').show();
     }
-    $('#title_wrap').show();
 
     $('.text_buttons').show();
     $(".navbar").hide();
     $("#wrapper").show();
-    $('#wrapper').addClass(gon.background);
-    $('#wrapper').css('font-family', gon.font);
+    $('#wrapper').addClass(gon.background).css('font-family', gon.font);
     init_widget();
 
     //change class of a wrapper (i.e. bg, theme) based on theme selection drop-down from theme modal, and display currently selected theme in theme modal
     $("#ts").live("change", function () {
         var selected_theme = $('#ts').val();
-        $('#wrapper').removeClass($('#wrapper').attr('class')).addClass(selected_theme);
+        $wrapper = $('#wrapper');
+        $wrapper.removeClass($wrapper.attr('class')).addClass(selected_theme);
         $("#current_theme_name").html("Current Theme " + ': ' + $('#ts option:selected').text());
         gon.background = selected_theme;
     });
 
     $('#theme_Modal').bind('show', function () {
-        $("#current_theme_name").html("Current Theme : ");
-        $("#current_theme_name").append($('#ts option:selected').text());
+        $("#current_theme_name").html("Current Theme : ").append($('#ts option:selected').text());
     });
 
     //change font of each block i.e. wrapper, title_wrap, text_wrap, caption_wrap block and display currently selected font in font modal
     $("#fs").live("change", function () {
+        var $font = $('#fs option:selected').text();
         var font_style = $('#fs').val();
         $('#wrapper').css('font-family', font_style);
-        $("#current_font_name").html("Current Font " + ': ' + $('#fs option:selected').text());
+        $("#current_font_name").html("Current Font " + ': ' + $font);
         var fontsize, h1fontsize;
         $.each($('.text_block'), function (index, elm) {
             fontsize = (parseInt($(elm).css("font-size").replace("px", "")) - gon.fontadjustment[index]) + "px";
@@ -47,7 +47,7 @@ $(function () {
             $(elm).css("font-size", fontsize);
             $(elm).find('h1').css("font-size", h1fontsize);
         });
-        gon.font = $('#fs option:selected').text();
+        gon.font = $font;
     });
 });
 
