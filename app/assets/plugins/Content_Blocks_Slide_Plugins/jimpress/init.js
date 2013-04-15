565d93453b7782bd11326efe329c9dba567a37b5
Arijit Lahiri <arijit@ptotem.com>
2013-04-15 05:55:43 +0530

    Got Imagecube and Jimpress working. Some more changes required, though.

--- app/assets/plugins/Content_Blocks_Slide_Plugins/jimpress/init.js
+++ app/assets/plugins/Content_Blocks_Slide_Plugins/jimpress/init.js
@@ -1,8 +1,5 @@
 function init_widget() {
 
-    $('#caption_wrap').hide();
-    $('#caption_wrap_back').hide();
-
     $.each(images, function (index, elm) {
         $('#jms-slideshow').append('<div class="step" data-color="color-' + Math.round(Math.random() * 4) + '" data-x="-' + Math.round(Math.random() * 150) + '" data-y="-' + Math.round(Math.random() * 150) + '" data-z="' + Math.round(Math.random() * 1500) + '" data-rotate="' + Math.round(Math.random() * 180) + '"><div class="jms-content"><h3>' + captions[index] + '</h3></div><img src="' + elm + '"/></div>');
 
@@ -12,25 +9,21 @@ function init_widget() {
             };
 
             $('#jms-slideshow').jmslideshow($.extend(true, { jmpressOpts: jmpressOpts }, {
-                autoplay: true,
+                autoplay: false,
                 bgColorSpeed: '0.8s',
-                arrows: false
+                arrows: true
             }));
         }
     });
 
     setTimeout(function () {
+        if (gon.no_subtitle) {
+            $('#subtitle_back').removeClass('elements');
+        }
         $('#wrapper').fadeIn(function () {
-            $('#widget_wrap').fadeIn('slow', function () {
-                $('#title_wrap').fadeIn();
-                $('#text_wrap_back').slideDown();
-                $('#text_wrap').slideDown(1000, function () {
-                    $('#text_wrap_back').slideDown();
-                    $('.navbar').fadeIn(function () {
-                        $('.ad-menu-item').slideToggle();
-                    });
-                });
-            });
+            $title.boxfit({multiline: true, maximum_font_size: 36});
+            $subtitle.boxfit({multiline: true, maximum_font_size: 36});
+            $('.elements').fade1by1();
         });
-    }, 1000);
+    }, 500);
 }
