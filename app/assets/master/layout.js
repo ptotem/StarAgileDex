7a7d1674045d084e580b434b02e4045f7e2402ca
Arijit Lahiri <arijit@ptotem.com>
2013-04-16 09:10:03 +0530

    Made some plugin level changes on the Powerpoint plugins. Also cleaned up the in-slide menu

--- app/assets/master/layout.js
+++ app/assets/master/layout.js
@@ -6,7 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-function layout1(){
+function layout1() {
     if (gon.no_subtitle && gon.no_titlepic) {
         $('#subtitle_back').removeClass('elements');
         $('#titlepic').removeClass('elements');
@@ -16,8 +16,8 @@ function layout1(){
         }).find('h1').css({
                 'lineHeight': "72px"
             });
-    }else{
-        if (gon.no_subtitle){
+    } else {
+        if (gon.no_subtitle) {
             $('#subtitle_back').removeClass('elements');
         }
     }
@@ -26,6 +26,39 @@ function layout1(){
             $title.boxfit({multiline: true, maximum_font_size: 36});
             $subtitle.boxfit({multiline: true, maximum_font_size: 36});
             $caption.html($cube.find('img:first').attr('title'));
+            $('.elements').fade1by1();
+        });
+    }, 500);
+}
+
+function layout2() {
+    if (gon.no_subtitle && gon.no_titlepic) {
+        $('#subtitle_back').removeClass('elements');
+        $('#titlepic').removeClass('elements');
+        $('#rotator').css({
+            'height': "300px",
+            'fontSize': "36px"
+        }).find('h1').css({
+                'lineHeight': "72px"
+            });
+    } else {
+        if (gon.no_subtitle) {
+            $('#subtitle_back').removeClass('elements');
+        }
+    }
+    setTimeout(function () {
+        $('#wrapper').fadeIn(function () {
+            $title.boxfit({multiline: true, maximum_font_size: 36});
+            $subtitle.boxfit({multiline: true, maximum_font_size: 36});
+            $caption.html($cube.find('img:first').attr('title'));
+            $('.elements').fade1by1();
+        });
+    }, 500);
+}
+
+function layout3() {
+    setTimeout(function () {
+        $('#wrapper').fadeIn(function () {
             $('.elements').fade1by1();
         });
     }, 500);
