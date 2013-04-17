02f410cd4b3575d552a6811efabf794ec8994731
ptotem <info@ptotem.com>
2013-04-17 21:02:49 +0530

    layout changed todo text fit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130415090442) do
+ActiveRecord::Schema.define(:version => 20130415135047) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(:version => 20130415090442) do
     t.integer  "ppt_file_size"
     t.datetime "ppt_updated_at"
     t.string   "mode",                  :default => "HTML"
+    t.boolean  "nosub",                 :default => false
   end
 
   create_table "users", :force => true do |t|
