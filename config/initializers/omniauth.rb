88f09d850e04000eff75e95c9464408b1b10e425
Nilesh <nilesh@ptotem.com>
2013-04-19 12:15:17 +0530

    Another twitter-app created for dev (newly created app is being used for dev, old is kept for server)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,6 +10,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #new for dev
   provider :facebook, '636324466384812', 'f65a3e41c1c8d6d97038cdf638a15387'
 
-  provider :twitter, 'KnWQoa66ltimDwt4SshMEA', 'cwcc6FWj3yuvMRcReSHQqfwGkYWasjuUIGRVA2vfbM'
+  ##old for server
+  #provider :twitter, 'KnWQoa66ltimDwt4SshMEA', 'cwcc6FWj3yuvMRcReSHQqfwGkYWasjuUIGRVA2vfbM'
+
+  #new for dev
+  provider :twitter, 'niPxGbAaE0VYqXbUbbqU2Q', 'T06zGce7p2hlOZuYLM1TqdJYTTlMwSXXQ6uCHBxKY'
+
 end
 
