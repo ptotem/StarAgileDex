85128d4ff4fa4c35e00e119de97875da9001f7b0
Nilesh <nilesh@ptotem.com>
2013-05-15 14:48:30 +0530

    fb login err corrected

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -16,10 +16,18 @@ class AuthenticationsController < ApplicationController
     require 'pathname'
     auth=request.env["omniauth.auth"]
     if auth.provider=='facebook' # Checking if request comes from facebook or twitter
-      if User.find_by_email(auth.info.email).nil?
-        @user = User.create!(:provider => auth.provider, :email => auth.info.email, :uid => auth.uid, :password => Devise.friendly_token[0, 20], :name => "#{auth.info.first_name} #{auth.info.last_name}")
+                                 #if User.find_by_email(auth.info.email).nil?
+      if User.find_by_uid(auth['uid']).nil?
+        users_email = auth.extra.raw_info.email
+        if users_email.nil?
+          @user = User.create(:provider => auth["provider"], :uid => auth["uid"], :name => auth["info"]["name"])
+        else
+          @user = User.create(:provider => auth["provider"], :email => users_email, :password => Devise.friendly_token[0, 20], :uid => auth["uid"], :name => auth["info"]["name"])
+        end
+
       else
-        @user=User.find_by_email(auth.info.email)
+        #@user=User.find_by_email(auth.info.email)
+        @user=User.find_by_uid(auth['uid'])
       end
     else
       if User.find_by_uid(auth['uid']).nil?
